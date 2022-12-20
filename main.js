96b9872ab27088e2f15256869f40f6b3d6ccb486
Alhassan Osman <oalhassan847@gmail.com>
2022-12-20 16:22:40 +0000

    add JS variables

---
+++ main.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector('.menu');
+const closeBtn = document.querySelector('#close');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuItem1 = document.querySelector('.mobile-menu-item1');
+const mobileMenuItem2 = document.querySelector('.mobile-menu-item2');
+const mobileMenuItem3 = document.querySelector('.mobile-menu-item3');
