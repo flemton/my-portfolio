ffa0e234b2be84cfc8d495366974334b283c8209
Alhassan Osman <oalhassan847@gmail.com>
2022-12-20 20:36:29 +0000

    document format and fix errors

--- main.js
+++ main.js
@@ -4,42 +4,41 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuItem = document.querySelector('.mobile-menu-item');
 const mobileMenuItem1 = document.querySelector('.mobile-menu-item1');
 const mobileMenuItem2 = document.querySelector('.mobile-menu-item2');
-const navbar = document.querySelector(".nav-bar")
-
+const navbar = document.querySelector('.nav-bar');
 
 function showMenu() {
-    mobileMenu.style.display = 'block';
-    closeBtn.style.display = 'block';
-    navbar.style.display = 'none';
-    menuBtn.style.display = 'none';
-  }
-  
-  function hideMenu() {
-    menuBtn.style.display = 'block';
+  mobileMenu.style.display = 'block';
+  closeBtn.style.display = 'block';
+  navbar.style.display = 'none';
+  menuBtn.style.display = 'none';
+}
+
+function hideMenu() {
+  menuBtn.style.display = 'block';
+  navbar.style.display = 'flex';
+  mobileMenu.style.display = 'none';
+  closeBtn.style.display = 'none';
+}
+
+menuBtn.addEventListener('click', showMenu);
+closeBtn.addEventListener('click', hideMenu);
+mobileMenuItem.addEventListener('click', hideMenu);
+mobileMenuItem1.addEventListener('click', hideMenu);
+mobileMenuItem2.addEventListener('click', hideMenu);
+
+function myFunction(desktopView) {
+  if (desktopView.matches) {
     navbar.style.display = 'flex';
     mobileMenu.style.display = 'none';
-    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'none';
+  } else {
+    menuBtn.style.display = 'block';
+    mobileMenu.style.display = 'none';
   }
-  
-  menuBtn.addEventListener('click', showMenu);
-  closeBtn.addEventListener('click', hideMenu);
-  mobileMenuItem.addEventListener('click', hideMenu);
-  mobileMenuItem1.addEventListener('click', hideMenu);
-  mobileMenuItem2.addEventListener('click', hideMenu);
+}
 
+const desktopView = window.matchMedia('(min-width: 768px)');
 
-  function myFunction(desktopView) {
-    if (desktopView.matches) {
-      mobileMenu.style.display = 'none';
-      menuBtn.style.display = 'none';
-    } else {
-      menuBtn.style.display = 'block';
-      mobileMenu.style.display = 'none';
-    }
-  }
-  
-  const desktopView = window.matchMedia('(min-width: 768px)');
-  
-  myFunction(desktopView);
-  
-  desktopView.addListener(myFunction);
+myFunction(desktopView);
+
+desktopView.addListener(myFunction);
