2f26211c2785327c784292652e039f0a285c8675
ashmalzahra <ashmal.zahraa@gmail.com>
2022-12-20 23:07:04 +0500

    Menu javascript functions

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuItem1 = document.querySelector('.mobile-menu-item1');
 const mobileMenuItem2 = document.querySelector('.mobile-menu-item2');
 const mobileMenuItem3 = document.querySelector('.mobile-menu-item3');
+const navbar = document.querySelector(".nav-bar")
+
+function showMenu(e) {
+    e.preventDefault();
+    mobileMenu.style.display = 'block';
+    navbar.style.display = 'none';
+  }
+  
+  function hideMenu() {
+    mobileMenu.style.display = 'none';
+  }
+  
+  menuBtn.addEventListener('click', showMenu);
+  closeBtn.addEventListener('click', hideMenu);
+  mobileMenuItem1.addEventListener('click', hideMenu);
+  mobileMenuItem2.addEventListener('click', hideMenu);
+  mobileMenuItem3.addEventListener('click', hideMenu);
