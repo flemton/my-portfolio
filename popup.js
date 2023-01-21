7b11887fac54ee0ea03dcf5be9818bae47be9563
Alhassan Osman <oalhassan847@gmail.com>
2023-01-21 04:33:26 +0000

    add todo-list to projects

--- popup.js
+++ popup.js
@@ -1,7 +1,4 @@
 // Works section
-// const portfolio = document.createElement('section');
-// portfolio.id = 'portfolio';
-// headline.append(portfolio);
 const works = document.createElement('article');
 works.className = 'works';
 
@@ -41,21 +38,21 @@ const Projects = [
     source: 'https://github.com/flemton/awesome-books-es6',
   },
   {
-    name: 'Tonic',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    featuredImage: './img/portfolio-tonic1.png',
+    name: 'Todo List',
+    description: '"To-do list" is a tool that helps to organize your day. It simply lists the things that you need to do and allows you to mark them as complete. built using ES6 and Webpack!',
+    featuredImage: './img/todo-list.png',
     details: {
-      DName: 'canopy',
-      skill: 'Back End Dev',
-      year: '2022',
+      DName: 'Todo',
+      skill: 'Front and Back End Dev',
+      year: '2023',
     },
     technologies: {
-      first: 'Html',
-      second: 'Css',
-      third: 'JavaScript',
+      first: 'ES6',
+      second: 'NodeJS',
+      third: 'Webpack',
     },
-    live: ' ',
-    source: ' ',
+    live: 'https://flemton.github.io/todo-list/dist/',
+    source: 'https://github.com/flemton/todo-list',
   },
   {
     name: 'Multi-Post Stories',
@@ -109,10 +106,62 @@ Projects.forEach((Project) => {
 
 document.body.insertBefore(works, document.body.children[3]);
 
-const popupDetails = document.querySelector('.popup');
+const popupDetails = document.createElement('section');
+popupDetails.className = 'popup';
+
 function showProjectDetails() {
   popupDetails.style.display = 'flex';
 }
+
+Projects.forEach((pop) => {
+  popupDetails.innerHTML = `
+    <div class="left-block">
+      <button type="button" class="close" id="popup-close"><i class="fa-solid fa-xmark fa-lg"></i></button>
+                <h2 class="popup-title">
+                    ${pop.name}
+                </h2>
+                <span class="canopy">
+                  ${pop.details.DName}
+                    <ul>
+                        <li><img src="./img/counter.png" alt=""></li>
+                        <li>${pop.details.skill}</li>
+                        <li><img src="./img/counter.png" alt=""></li>
+                        <li>${pop.details.year}</li>
+                    </ul>
+                </span>
+                <div class="detail-img">
+                    <img src="${pop.featuredImage}" alt="${pop.name} Image">
+                </div>
+                    <div class="detail-container">
+                        <p>${pop.description}</p>
+                            <div>
+                                <ul class="portfolio-tags">
+                                    <li>
+                                    ${pop.technologies.first}
+                                    </li>
+                                    <li>
+                                    ${pop.technologies.second}
+                                    </li>
+                                    <li>
+                                    ${pop.technologies.third}
+                                    </li>
+                                </ul>
+                                <div class="link-buttons">
+                                    <button type="button" class="project-links" onclick="location.href = '${pop.live}';">See Live
+                                        <i><img src="./img/circle-icon.png" alt=""></i>
+                                    </button>
+                                    <button type="button" class="project-links" onclick="location.href = '${pop.source}';" id="see-source">See Source
+                                        <i><img src="./img/github-blue.png" alt=""></i>
+                                    </button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+      </div>
+    `
+  document.body.insertBefore(popupDetails, document.body.children[2]);
+});
+
 
 const detailsButton = document.querySelectorAll('.project-links');
 detailsButton.forEach((button) => {
